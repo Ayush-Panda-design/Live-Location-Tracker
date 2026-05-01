7f350d4ab9bb64535f4231aeede83d86e5f0f7a5
Ayush-Panda-design <pandaayush25305@gmail.com>
2026-05-01 23:52:14 +0530

    Update kafka.js

--- src/kafka/kafka.js
+++ src/kafka/kafka.js
@@ -1,8 +1,27 @@
-const { Kafka } = require('kafkajs');
+/*const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
   clientId: 'live-location-tracker',
   brokers: [process.env.KAFKA_BROKER]
+});
+
+module.exports = kafka;*/
+
+const { Kafka } = require('kafkajs');
+
+const kafka = new Kafka({
+  clientId: 'live-location-tracker',
+
+  brokers: [process.env.KAFKA_BROKER],
+
+  // 🔴 REQUIRED for Aiven
+  ssl: true,
+
+  sasl: {
+    mechanism: 'plain',
+    username: process.env.KAFKA_USERNAME,
+    password: process.env.KAFKA_PASSWORD,
+  },
 });
 
 module.exports = kafka;
