51b544a4edf04d5b6ef2bd457327c29ad29c6410
Ayush-Panda-design <pandaayush25305@gmail.com>
2026-05-01 23:55:53 +0530

    Update kafka.js

--- src/kafka/kafka.js
+++ src/kafka/kafka.js
@@ -8,14 +8,15 @@ const kafka = new Kafka({
 module.exports = kafka;*/
 
 const { Kafka } = require('kafkajs');
+const fs = require('fs');
 
 const kafka = new Kafka({
   clientId: 'live-location-tracker',
-
   brokers: [process.env.KAFKA_BROKER],
 
-  // 🔴 REQUIRED for Aiven
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false, // 🔥 FIX FOR RENDER / NODE SSL ISSUE
+  },
 
   sasl: {
     mechanism: 'plain',
