e8a615c12e5a5d951bb64c6e5d027d948025602e
Ayush-Panda-design <pandaayush25305@gmail.com>
2026-05-01 23:38:13 +0530

    Update kafka.js

--- src/kafka/kafka.js
+++ src/kafka/kafka.js
@@ -2,7 +2,7 @@ const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
   clientId: 'live-location-tracker',
-  brokers: [process.env.KAFKA_BROKER || 'localhost:29092']
+  brokers: [process.env.KAFKA_BROKER]
 });
 
 module.exports = kafka;
